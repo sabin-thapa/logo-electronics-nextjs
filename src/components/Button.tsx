b1bef0fddfe240f3ada0f36dae6faac321b926ff
Sabin Thapa <sabint017@gmail.com>
2023-06-06 20:31:12 +0545

    feat(component: button): make reusable button
    the same button can be used as per the requirements -- can be used with linear gradient  text and
    white text too

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,15 +2,27 @@ import styles from "@/styles/Button.module.css";
 import classNames from "classnames";
 import React from "react";
 
-const Button: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = ({
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  fontColor?: string;
+}
+
+const Button: React.FC<ButtonProps> = ({
   children,
   className,
+  fontColor,
   ...props
 }) => {
-
-
   return (
-    <button className={classNames("text-white px-6 py-2 font-poppins font-bold text-lg", className, styles.btn)} {...props}>
+    <button
+      className={classNames(
+        styles.btn,
+        "text-white px-6 py-2 font-poppins font-bold text-lg",
+        { [styles.gradient_font]: !fontColor },
+        className
+      )}
+      style={{ color: fontColor }}
+      {...props}
+    >
       {children}
     </button>
   );
