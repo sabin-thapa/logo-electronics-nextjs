3cbc53e68f3b9ccedb1295d98dea56e2b48920f9
Sabin Thapa <sabint017@gmail.com>
2023-06-07 18:44:00 +0545

    feat: add rocket launcher
    this rocket icon will later be used to scroll to the top of the page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,76 +9,86 @@ const Footer = () => {
     "uppercase font-poppins text-[#D4C9C9] text-xl tracking-widest py-2";
 
   return (
-    <div className="relative">
-      <div className={styles.footer_image_section}>
+    <>
+      <div className="flex justify-end mr-36 -mb-24 z-10 relative">
         <Image
-          className={classNames(styles.footer_image, styles.overlay_image)}
-          src="/assets/footer_bg_overlay.png"
-          alt="Image 1"
-          width={800}
-          height={600}
-        />
-        <Image
-          className={styles.footer_image}
-          src="/assets/footer_bg.png"
-          alt="Image 2"
-          width={800}
-          height={600}
+          src="/assets/rocket.svg"
+          alt="Rocket Image"
+          width={100}
+          height={50}
         />
       </div>
+      <div className="relative">
+        <div className={styles.footer_image_section}>
+          <Image
+            className={classNames(styles.footer_image, styles.overlay_image)}
+            src="/assets/footer_bg_overlay.png"
+            alt="Image 1"
+            width={800}
+            height={600}
+          />
+          <Image
+            className={styles.footer_image}
+            src="/assets/footer_bg.png"
+            alt="Image 2"
+            width={800}
+            height={600}
+          />
+        </div>
 
-      <div className="absolute inset-0 flex flex-col justify-center ">
-        <div className="grid grid-cols-3 justify-around gap-36 place-ite">
-          <div className="text-white mx-4 flex-grow pl-20">
-            <Image
-              src="/assets/main_logo.png"
-              width={100}
-              height={100}
-              alt="Logo of Logo Electronics"
-              priority={true}
-            />
+        <div className="absolute inset-0 flex flex-col justify-center ">
+          <div className="grid grid-cols-3 justify-around gap-36 place-ite">
+            <div className="text-white mx-4 flex-grow pl-20">
+              <Image
+                src="/assets/main_logo.png"
+                width={100}
+                height={100}
+                alt="Logo of Logo Electronics"
+                priority={true}
+              />
 
-            <p className="font-poppins mt-12 font-normal tracking-[0.25em] leading-7">
-              lOGO is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry&#39;s standard dummy
-              text ever since the 1500s, when
-            </p>
-          </div>
-          <div className="text-white mx-4 flex-grow ">
-            <SubHeader text="Useful Links" className="mt-4" />
-            <div className="flex flex-col pl-7 ">
-              <h1 className={faintFooterStyle}>Home</h1>
-              <h1 className={faintFooterStyle}>Product</h1>
-              <h1 className={faintFooterStyle}>About us</h1>
-              <h1 className={faintFooterStyle}>contact</h1>
+              <p className="font-poppins mt-12 font-normal tracking-[0.25em] leading-7">
+                lOGO is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry&#39;s standard dummy
+                text ever since the 1500s, when
+              </p>
             </div>
-          </div>
-          <div className="text-white mx-4 flex-grow relative">
-            <Image
-              src="/assets/follow_bg.svg"
-              height={340}
-              width={320}
-              alt="follow us"
-            />
-            <div className="absolute top-4 left-20   ">
-              <div className="relative">
-                <SubHeader text="Follow Us On" fontColor="#fff" />
+            <div className="text-white mx-4 ml-36 flex-grow ">
+              <SubHeader text="Useful Links" className="mt-4" />
+              <div className="flex flex-col pl-7 ">
+                <h1 className={faintFooterStyle}>Home</h1>
+                <h1 className={faintFooterStyle}>Product</h1>
+                <h1 className={faintFooterStyle}>About us</h1>
+                <h1 className={faintFooterStyle}>contact</h1>
               </div>
             </div>
-            <div className="flex gap-14 mt-6">
-              <FaFacebook size={48} />
-              <FaInstagram size={48} />
-              <FaTwitter size={48} />
-              <FaYoutube size={48} />
+            <div className="text-white mx-4 flex-grow relative">
+              <Image
+                src="/assets/follow_bg.svg"
+                height={200}
+                width={250}
+                alt="follow us"
+              />
+              <div className="absolute top-2 left-11">
+                <div className="relative">
+                  <SubHeader text="Follow Us On" fontColor="#fff" />
+                </div>
+              </div>
+              <div className="flex gap-9 mt-6">
+                <FaFacebook size={48} />
+                <FaInstagram size={48} />
+                <FaTwitter size={48} />
+                <FaYoutube size={48} />
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <div className="copyrights_section flex min-h-[6rem] tracking-widest uppercase font-poppins text-center text-white justify-center items-center">
-        <p>Copyright 2021 lOGO ALL RIGHT RESERVED </p>
+        <div className="copyrights_section flex min-h-[6rem] tracking-widest uppercase font-poppins text-center text-white justify-center items-center">
+          <p>Copyright 2021 lOGO ALL RIGHT RESERVED </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
